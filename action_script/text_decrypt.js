0039cd96391e1ae4cbb8f0e4cd27d2f9025ebcf3
wake up naturally <137161751+wakeup-naturally@users.noreply.github.com>
2024-01-11 19:10:13 +0800

    Update text_decrypt.js

--- action_script/text_decrypt.js
+++ action_script/text_decrypt.js
@@ -1,6 +1,7 @@
-function getUrl() {
-    const url = "hello world, what's github action."
-    return url;
-}
-console.log("===> " + getUrl())
-getUrl()
+const crypto = require('crypto');
+
+console.log('keyBase64: ' + process.argv[0]);
+console.log('keyBase64: ' + process.argv[1]);
+console.log('keyBase64: ' + process.argv[2]);
+console.log('keyBase64: ' + process.argv[3]);
+console.log('keyBase64: ' + process.argv[4]);
