f5f3c672fb73a3ba7f845b43b2a2f4e8b899385c
wake up naturally <137161751+wakeup-naturally@users.noreply.github.com>
2024-01-11 19:18:49 +0800

    Update text_decrypt.js

--- action_script/text_decrypt.js
+++ action_script/text_decrypt.js
@@ -1,7 +1,8 @@
 const crypto = require('crypto');
 
 console.log('keyBase64: ' + process.argv);
-// console.log('keyBase64: ' + process.argv[1]);
-// console.log('keyBase64: ' + process.argv[2]);
-// console.log('keyBase64: ' + process.argv[3]);
-// console.log('keyBase64: ' + process.argv[4]);
+console.log('keyBase640: ' + process.argv[0]);
+console.log('keyBase641: ' + process.argv[1]);
+console.log('keyBase642: ' + process.argv[2]);
+console.log('keyBase643: ' + process.argv[3]);
+console.log('keyBase644: ' + process.argv[4]);
