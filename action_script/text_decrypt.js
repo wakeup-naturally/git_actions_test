f5a70aa1cba3d6f962e4c5d24803a2406da59b92
wake up naturally <137161751+wakeup-naturally@users.noreply.github.com>
2024-01-12 09:55:17 +0800

    Update text_decrypt.js

--- action_script/text_decrypt.js
+++ action_script/text_decrypt.js
@@ -2,7 +2,11 @@ const crypto = require('crypto');
 const fs = require('fs')
  require('dotenv').config()
 
-console.log(process.env.HELLO_PRIVATE_KEY.replace(/\\n/g, '\n'))
+//console.log(process.env.HELLO_PRIVATE_KEY.replace(/\\n/g, '\n'))
+
+fs.readFile("./.private_key.pem", function(err, data) {
+    err ? console.log(data) : console.log("读取失败 " + err)
+})
 
 // console.log('keyBase64: ' + process.argv[1]);
 // console.log('keyBase64: ' + process.argv[2]);
